17f02bb03a351fd7684b50114cce935cd07d4221
Dan Hart <dan-hart@users.noreply.github.com>
2023-08-25 12:29:02 -0500

    Colorful

--- GridExploration/ContentView.swift
+++ GridExploration/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors = [
+        Color.red,
+        Color.blue,
+        Color.green,
+        Color.yellow,
+        Color.purple,
+        Color.orange,
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Grid {
+            GridRow(alignment: .top) {
+                ColorSquare(color: colors[0])
+                    .gridCellColumns(2)
+                
+                VStack {
+                    ColorSquare(color: colors[1])
+                    ColorSquare(color: colors[3])
+                }
+            }
+            .frame(height: 400)
+
+            GridRow {
+                ColorSquare(color: colors[2])
+                ColorSquare(color: colors[4])
+                ColorSquare(color: colors[5])
+            }
+            .frame(height: 200)
         }
         .padding()
+    }
+}
+
+struct ColorSquare: View {
+    let color: Color
+        
+    var body: some View {
+        color
     }
 }
 
