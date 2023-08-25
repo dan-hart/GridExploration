0822e47e12e7a67c70f51d3222202cb917510717
Dan Hart <dan-hart@users.noreply.github.com>
2023-08-25 11:57:44 -0500

    Initial Commit

---
+++ GridExploration/GridExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridExplorationApp.swift
+//  GridExploration
+//
+//  Created by Dan Hart on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct GridExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
